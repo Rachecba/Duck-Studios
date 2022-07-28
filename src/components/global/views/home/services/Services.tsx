ff2e3a66afd07f3ff19a3cacfb35029760c24347
Rachel <rachbasulto@gmail.com>
2022-07-28 00:43:14 -0600

    services homepage events

--- src/components/global/views/home/services/Services.tsx
+++ src/components/global/views/home/services/Services.tsx
@@ -4,9 +4,32 @@ import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 import { designIcon, marketingIcon, softwareIcon } from "../../../../../utils/constants/constants"
 import * as Styled from './Services.style'
+import ReactGA from 'react-ga4';
 
 function Services() {
-    const { t, i18n, ready } = useTranslation(['home','global'], { useSuspense: true });
+    const { t, i18n, ready } = useTranslation(['home', 'global'], { useSuspense: true });
+    
+    const softwareServicesEvent = () => { 
+        ReactGA.event({
+            category: 'Homepage: Software Services',
+            action: `See Software Services`,
+        });
+    }
+
+    const marketingServicesEvent = () => { 
+        ReactGA.event({
+            category: 'Homepage: Marketing Services',
+            action: `See Marketing Services`,
+        });
+    }
+
+    const designServicesEvent = () => { 
+        ReactGA.event({
+            category: 'Homepage: Design Services',
+            action: `See Design Services`,
+        });
+    }
+
     return (
         <Styled.Container id="services">
             <Styled.SectionTitle>
@@ -25,7 +48,7 @@ function Services() {
                         <h1>{t('home.ourServices.software.title')}</h1>
                         <p>{t('home.ourServices.software.description')}</p>
                         <Link to={"/software"}>
-                        <Button className="button">{t('global:global.btnSeeMore')}</Button>
+                        <Button className="button" onClick={softwareServicesEvent}>{t('global:global.btnSeeMore')}</Button>
                         </Link>
                     </Styled.Card>
                     <Styled.Card className="middle-card">
@@ -35,7 +58,7 @@ function Services() {
                         <h1>{t('home.ourServices.marketing.title')}</h1>
                         <p>{t('home.ourServices.marketing.description')}</p>
                         <Link to={"/marketing"}>
-                        <Button className="button">{t('global:global.btnSeeMore')}</Button>
+                        <Button className="button" onClick={marketingServicesEvent}>{t('global:global.btnSeeMore')}</Button>
                         </Link>
                     </Styled.Card>
                     <Styled.Card className="end-card">
@@ -45,7 +68,7 @@ function Services() {
                         <h1>{t('home.ourServices.graphicDesign.title')}</h1>
                         <p>{t('home.ourServices.graphicDesign.description')}</p>
                         <Link to={"/graphic-design"}>
-                        <Button className="button">{t('global:global.btnSeeMore')}</Button>
+                        <Button className="button" onClick={designServicesEvent}>{t('global:global.btnSeeMore')}</Button>
                         </Link>
                     </Styled.Card>
                 </Styled.CardsContainer>
